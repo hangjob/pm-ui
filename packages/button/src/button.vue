bf32c726fbf9281823a930e45a57648a850a6b66
yanghang <yanghang@pminfo.cn>
2021-08-16 18:11:54 +0800

    过滤lib文件

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -13,8 +13,8 @@
                 'is-loading': loading,
                 'is-plain': plain,
                 'is-round': round,
-                'is-circle': circle,
-            },
+                'is-circle': circle
+            }
         ]"
     >
         <i class="pm-icon-loading" v-if="loading"></i>
@@ -23,34 +23,34 @@
     </button>
 </template>
 <script>
-import { throttle, debounce } from 'lodash'
-import { isDefined } from '~@/utils/types'
+import { throttle, debounce } from "lodash"
+import { isDefined } from "~@/utils/types"
 
 export default {
-    name: 'PmButton',
+    name: "PmButton",
     // 表单 读取父组件值
     inject: {
         pmForm: {
-            default: '',
+            default: ""
         },
         pmFormItem: {
-            default: '',
-        },
+            default: ""
+        }
     },
 
     props: {
         type: {
             type: String,
-            default: 'default',
+            default: "default"
         },
         size: String,
         icon: {
             type: String,
-            default: '',
+            default: ""
         },
         nativeType: {
             type: String,
-            default: 'button',
+            default: "button"
         },
         loading: Boolean,
         disabled: Boolean,
@@ -59,53 +59,58 @@ export default {
         round: Boolean,
         circle: Boolean,
         debounce: [Boolean, Object],
-        throttle: [Boolean, Object],
+        throttle: [Boolean, Object]
     },
 
     computed: {
-        _pmFormItemSize () {
+        _pmFormItemSize() {
             return (this.pmFormItem || {}).pmFormItemSize
         },
-        buttonSize () {
-            return (
-                this.size || this._pmFormItemSize || (this.$PMUI || {}).size
-            )
+        buttonSize() {
+            return this.size || this._pmFormItemSize || (this.$PMUI || {}).size
         },
-        buttonDisabled () {
+        buttonDisabled() {
             return this.disabled || (this.pmForm || {}).disabled
-        },
+        }
     },
-    created () {
+    created() {
         if (this.debounce) {
             let options = {
                 wait: this.debounce.wait || 200,
                 leading: isDefined(this.debounce.leading) ? this.debounce.leading : true,
-                trailing: isDefined(this.debounce.trailing) ? this.debounce.trailing : false,
+                trailing: isDefined(this.debounce.trailing) ? this.debounce.trailing : false
             }
-            this._handleClick = debounce(function (evt) {
-                this.$emit('click', evt)
-            }, options.wait, options)
+            this._handleClick = debounce(
+                function (evt) {
+                    this.$emit("click", evt)
+                },
+                options.wait,
+                options
+            )
         }
         if (this.throttle) {
             let options = {
                 wait: this.throttle.wait || 1000,
                 leading: isDefined(this.throttle.leading) ? this.throttle.leading : true,
-                trailing: isDefined(this.throttle.trailing) ? this.throttle.trailing : false,
+                trailing: isDefined(this.throttle.trailing) ? this.throttle.trailing : false
             }
-            this._handleClick = throttle(function (evt) {
-                this.$emit('click', evt)
-            }, options.wait, options)
+            this._handleClick = throttle(
+                function (evt) {
+                    this.$emit("click", evt)
+                },
+                options.wait,
+                options
+            )
         }
     },
     methods: {
-        handleClick (evt) {
+        handleClick(evt) {
             if (!this.debounce && !this.throttle) {
-                this.$emit('click', evt)
-            }
-            else {
+                this.$emit("click", evt)
+            } else {
                 this._handleClick(evt)
             }
-        },
-    },
+        }
+    }
 }
 </script>
