4e3d452b9087e1f35b1c7847f8609fde85d359bc
yanghang <yanghang@pminfo.cn>
2021-11-18 15:23:40 +0800

    添加测试过滤文件

--- examples/pages/demtest/App.vue
+++ examples/pages/demtest/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <pm-image :src="url"></pm-image>
+        <pm-button>测试</pm-button>
     </div>
 </template>
 
@@ -9,7 +9,7 @@ export default {
     name: 'App',
     data() {
         return {
-            url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+
         }
     },
 }
